ecc967410c8c0ed913a7cde5160a65633ee8d0cb
22bipp22 <tammibippert@gmail.com>
2020-11-04 14:29:08 -0600

    added circles to map

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,7 +1,7 @@
 // define our map!!!
 let myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5
+    center: [37.09, -100.71],
+    zoom: 4
 });
 
 // copy pasta of the tile layer
@@ -19,22 +19,43 @@ let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 
 d3.json(queryUrl, function(response) {
     // console.log(response);
-    // console.log(response.features);
+    console.log(response.features);
     features = response.features;
     
     let heatArray = [];
     for (let i = 0; i < features.length; i++) {
         let location = features[i].geometry;
-        if (location) {
-            // console.log(location.coordinates[0])
-            heatArray.push([location.coordinates[1], location.coordinates[0]])
+        let properties = features[i].properties;
+        
+    
+        let color = "";
+        if (location.coordinates[2] > 100) {
+            color = "red";
         }
+        else if (location.coordinates[2] > 50) {
+            color = 'orange';
+        }
+        else if (location.coordinates[2] > 20) {
+            color = "yellow";
+        }
+        else if (location.coordinates[2] > 10) {
+            color = "green";
+        }
+        else {
+            color = "yellowgreen";
+        }
+        
+
+        L.circle([location.coordinates[1], location.coordinates[0]], {
+            fillOpacity: 0.95,
+            color: "black",
+            fillColor: color,
+            // adjust radius
+            radius: properties.mag * 20000
+            }).bindPopup("<h2>" + properties.place + "</h2> <hr> <h3> " + "Magnitude: " + properties.mag + "</h3> <hr> <h3> Depth: " + location.coordinates[2] + "</h3>").addTo(myMap);
+            
+        
     }
 
-    console.log(heatArray)
-
-    let heat = L.heatLayer(heatArray, {
-        radius: 50,
-        blur: 15
-    }).addTo(myMap);
+   
 });
